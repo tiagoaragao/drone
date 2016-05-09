c239d1d31b7e6570392456522c2ffdc2fbab1890
tiago <tiagojavaprogramador@gmail.com>
2016-05-09 15:26:29 -0300

    Testando

---
+++ src/tiagojavaprogramador/drone/controller/GetVideoInfo.java
@@ -0,0 +1,9 @@
+package tiagojavaprogramador.drone.controller;
+/**  @author Tiago Alexandre Soares Aragão - tiagojavaprogramador@gmail.com - http://tiagojavaprogramador.blogspot.com */
+
+public class GetVideoInfo {
+
+    public static void main(String[] args){
+        
+    }
+}
---
+++ src/tiagojavaprogramador/drone/model/Video.java
@@ -0,0 +1,9 @@
+package tiagojavaprogramador.drone.model;
+/**  @author Tiago Alexandre Soares Aragão - tiagojavaprogramador@gmail.com - http://tiagojavaprogramador.blogspot.com */
+
+public class Video {
+
+    public static void main(String[] args){
+        
+    }
+}
