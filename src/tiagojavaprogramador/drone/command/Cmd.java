56a4cc01b4214897d97dd816880e442b69be3629
tiago <tiagojavaprogramador@gmail.com>
2016-05-09 15:44:34 -0300

    Testando

--- src/tiagojavaprogramador/drone/command/Cmd.java
+++ src/tiagojavaprogramador/drone/command/Cmd.java
@@ -1,9 +1,68 @@
 package tiagojavaprogramador.drone.command;
-/**  @author Tiago Alexandre Soares Aragão - tiagojavaprogramador@gmail.com - http://tiagojavaprogramador.blogspot.com */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
+
+/**
+ * @author Tiago Alexandre Soares Aragão - tiagojavaprogramador@gmail.com -
+ * http://tiagojavaprogramador.blogspot.com
+ */
 public class Cmd {
 
-    public static void main(String[] args){
-        
+    private String resposta = "";
+    private String comando = "";
+    private String line;
+
+    public String comandosDownload(String comandos) {
+        this.comando = comandos;
+        Thread tw = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    
+                    ProcessBuilder builders = new ProcessBuilder("cmd.exe", "/c", comando);
+                    builders.redirectErrorStream(true);
+                    Process z = builders.start();
+                    BufferedReader rs = new BufferedReader(new InputStreamReader(z.getInputStream()));
+                 
+                    String multipleLine = "";
+                    boolean tentativas = false;
+                    while (true) {
+                        line = rs.readLine();
+                        if (line.contains("ERROR:") || line.contains("conex�o") || line.contains("URLError")) {
+
+                            resposta = "type_error=Cheque a conexao!";
+                            tentativas = true;
+
+                        }
+                        if (line.contains("has already been downloaded")) {
+                            resposta = "type_error=Este video ja foi baixado!";
+                        }
+                        if (line.contains("%")) {
+                            float porcent = Float.parseFloat(line.substring(line.indexOf("]"), line.lastIndexOf("%")).replace(" ", "").replace("]", "").replace("%", ""));
+
+                            if (line.equals("100")) {
+                                resposta = "type_percent=Download concluido!";
+                            }else{
+                                 resposta = "type_percent=" + line;
+                            }
+                        }
+
+                        if (line == null) {
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    resposta = "type_error=";
+                } catch (NullPointerException e) {
+                    resposta = "type_error=";
+                }
+            }
+        });
+        tw.start();
+
+        return resposta;
     }
 }
